d1ea7d058ed215fcc0f441937b71b1c01c10eb2c
Clever0123 <1906956872@qq.com>
2020-08-16 17:26:39 +0800

    。。

--- assets/Script/loadingScene.ts
+++ assets/Script/loadingScene.ts
@@ -6,12 +6,12 @@ export default class LoadingScene extends cc.Component {
    // loadbtn: cc.Button = null
         
     start () {
-        let loadbtn = this.node.getChildByName('loadBtn')
+        /*let loadbtn = this.node.getChildByName('loadBtn')
         loadbtn.on(cc.Node.EventType.TOUCH_END, () => {
             this.node.runAction(cc.sequence(cc.fadeOut(0.5), cc.callFunc( () =>{
                 cc.director.loadScene("mainScene")
                 })))
-        })
+        })*/
     // setTimeout(() => {
     //     cc.director.loadScene("mainScene", () => {
     //         console.log("进入主游戏场景")
@@ -19,6 +19,11 @@ export default class LoadingScene extends cc.Component {
     // },2000)
     // cc.TransitionFad.create(200,"mainScene",cc.color(0,0,0))
     
-    
     }
+    btngosence(){
+        this.node.runAction(cc.sequence(cc.fadeOut(0.5), cc.callFunc( () =>{
+            cc.director.loadScene("mainScene")
+            })))
+    }
+    
 }
